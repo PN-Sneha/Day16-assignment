a060c70873ee53317d31a8ff98b622c3799cad4d
PN-Sneha <125186963+PN-Sneha@users.noreply.github.com>
2023-02-18 08:54:55 +0530

    Add files via upload

---
+++ fac.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+intmain()
+{
+	int i,fact=1;
+	printf("enter i");
+	scanf("%d",&i);
+	while(i>=1)
+	{
+		fact=fact*i;
+	}
+	printf("the factorial of a given number is %d");
+	return 0;
+}
